39373f49f8ab1f9f0cadb8ecdaec6b2bb2be5471
Rafał Zaborowski <raffik2000@gmail.com>
2021-10-24 14:04:16 +0200

    add routing to app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import Logo from './logo.png';
-import Money from './money.png';
-import Button from './components/UI/Button/Button';
+import { Route, Switch } from 'react-router';
+
+import Home from './pages/Home/Home';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="container mt-2">
-      <div className="d-flex align-items-center justify-content-center">
-        <img className="image mt-3 mb-3" src={Logo} alt="Logo" width="400" height="400" />
-      </div>
-      <div>
-        <div className="d-flex justify-content-around">
-          <img className="" src={Money} alt="Logo" width="200" height="200" />
-          <div>
-            <div className="d-flex align-items-center justify-content-center">
-              <Button onClick={() => {console.log("start")}}>Zacznij grę</Button>
-            </div>
-            <div className="d-flex align-items-center justify-content-center">
-              <Button onClick={() => {console.log("statystyki")}}>Statystyki</Button>
-            </div>
-            <div className="d-flex align-items-center justify-content-center">
-              <Button onClick={() => {console.log("dodaj")}}>Dodaj pytanie</Button>
-            </div>
-          </div>
-          <img className="" src={Money} alt="Logo" width="200" height="200" />
-        </div>
-      </div>
-    </div>
+    <Route>
+      <Switch>
+        <Route path='/' component={Home}/>
+      </Switch>
+    </Route>
+
   );
 }
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,35 @@
+import Logo from '../../logo.png';
+import Money from '../../money.png';
+import Button from '../../components/UI/Button/Button';
+import './Home.css';
+import { useHistory } from 'react-router';
+
+function Home() {
+    const history = useHistory()
+    return (
+        <div className="container mt-2">
+            <div className="d-flex align-items-center justify-content-center">
+                <img className="image mt-3 mb-3" src={Logo} alt="Logo" width="400" height="400" />
+            </div>
+            <div>
+                <div className="d-flex justify-content-around">
+                    <img className="" src={Money} alt="Logo" width="200" height="200" />
+                    <div>
+                        <div className="d-flex align-items-center justify-content-center">
+                            <Button onClick={() => { history.push('/play') }}>Zacznij grę</Button>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-center">
+                            <Button onClick={() => { history.push('/statistics') }}>Statystyki</Button>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-center">
+                            <Button onClick={() => { history.push('/add-question') }}>Dodaj pytanie</Button>
+                        </div>
+                    </div>
+                    <img className="" src={Money} alt="Logo" width="200" height="200" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
