41dbe09dea419d5f8fbd067c83263df0c67f6372
Rafał Zaborowski <raffik2000@gmail.com>
2021-11-03 18:35:32 +0100

    add capture value in input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,9 +2,9 @@ import './Input.scss';
 
 const Input = (props) => {
     return (
-        <div class="form__group field">
-            <input type="input" class="form__field" placeholder={props.children} required />
-            <label for="name" class="form__label">{props.children}</label>
+        <div className="form__group field">
+            <input onChange={props.onChange} value={props.value} type="input" className="form__field" placeholder={props.children} required />
+            <label for="name" className="form__label">{props.children}</label>
         </div>
     );
 }
--- src/pages/AddQuestionForm/AddQuestionFrom.js
+++ src/pages/AddQuestionForm/AddQuestionFrom.js
@@ -1,4 +1,4 @@
-
+import  { React, useState } from "react";
 import { useHistory } from "react-router";
 import ButtonInput from "../../components/UI/ButtonInput/ButtonInput";
 import Input from "../../components/UI/Input/Input";
@@ -6,6 +6,12 @@ import Input from "../../components/UI/Input/Input";
 import './AddQuestionFrom.css';
 
 const AddQuestionForm = (props) => {
+    const [question, setQuestion] = useState('')
+    const [correctAnswer, setCorrectAnswer] = useState('')
+    const [incorrectAnswer1, setIncorrectAnswer1] = useState('')
+    const [incorrectAnswer2, setIncorrectAnswer2] = useState('')
+    const [incorrectAnswer3, setIncorrectAnswer3] = useState('')
+
     const history = useHistory()
     return (
         <div className="container mt-3 mb-3">
@@ -14,19 +20,29 @@ const AddQuestionForm = (props) => {
                 <div>
                     <h2>Dodaj pytanie</h2>
                     <div className="d-flex align-items-center justify-content-center">
-                        <Input>Question</Input>
+                        <Input value={question} onChange={(event) => {
+                            setQuestion(event.target.value);
+                        }}>Question</Input>
                     </div>
                     <div className="d-flex align-items-center justify-content-center">
-                        <Input>Correct answer</Input>
+                        <Input value={correctAnswer} onChange={(event) => {
+                            setCorrectAnswer(event.target.value);
+                        }}>Correct answer</Input>
                     </div>
                     <div className="d-flex align-items-center justify-content-center">
-                        <Input>Incorrect answer</Input>
+                        <Input value={incorrectAnswer1} onChange={(event) => {
+                            setIncorrectAnswer1(event.target.value);
+                        }}>Incorrect answer</Input>
                     </div>
                     <div className="d-flex align-items-center justify-content-center">
-                        <Input>Incorrect answer</Input>
+                        <Input value={incorrectAnswer2} onChange={(event) => {
+                            setIncorrectAnswer2(event.target.value);
+                        }}>Incorrect answer</Input>
                     </div>
                     <div className="d-flex align-items-center justify-content-center">
-                        <Input>Incorrect answer</Input>
+                        <Input value={incorrectAnswer3} onChange={(event) => {
+                            setIncorrectAnswer3(event.target.value);
+                        }}>Incorrect answer</Input>
                     </div>
                 </div>
                 <div>
