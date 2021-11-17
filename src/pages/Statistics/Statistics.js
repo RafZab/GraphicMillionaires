603c1fc168411a840fb6a1283bcfde20503f6872
Rafał Zaborowski <raffik2000@gmail.com>
2021-11-17 21:59:04 +0100

    add button back

--- src/pages/Statistics/Statistics.js
+++ src/pages/Statistics/Statistics.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Card, Row, Col } from 'react-bootstrap';
 import axios from "../../axios";
 import Statistic from './components/Statistic/Statistic';
+
+import top from './top.jpg'
+import backArrow from './arrow-left-circle.svg'
+
+import './Statistics.css';
 
 const Statistics = () => {
     const [statistics, setStatistics] = useState(null)
@@ -34,19 +41,36 @@ const Statistics = () => {
     return (
         <div className="container mt-3">
             <div className="card">
-                <table className="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col"></th>
-                            <th scope="col">Nick</th>
-                            <th scope="col">Kwota</th>
-                            <th scope="col">Data</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {contextStatistics}
-                    </tbody>
-                </table>
+                <Card>
+                    <Card.Body>
+                        <Row>
+                            <Col xs="2">
+                                <NavLink to="/" className="text-back">
+                                    <span><img src={backArrow} alt="back" /> powrót do menu</span>
+                                </NavLink>
+                            </Col>
+                            <Col xs="8" className="d-flex align-items-center justify-content-center">
+                                <img src={top} alt="TOP 10" />
+                            </Col>
+                            <Col xs="2">
+                            </Col>
+                        </Row>
+
+                        <table className="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col">Nick</th>
+                                    <th scope="col">Kwota</th>
+                                    <th scope="col">Data</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {contextStatistics}
+                            </tbody>
+                        </table>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     );
